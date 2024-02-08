4c0ed38d1610557b2f05329c3236bc784a5f61f5
keerthi-koller <keerthi.nk8@gmail.com>
2024-02-08 10:03:22 +0530

    Modified: customer new address component

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,6 +1,6 @@
-function ErrorComponent () {
+function ErrorComponent() {
     return (<>
-        <h1 className="text-[100px] flex justify-center mt-[200px] text-red-600 font-bold">Error Occured</h1>
+        <h1 className="text-[99px] flex justify-center mt-[200px] text-red-600 font-bold">Error Occured</h1>
     </>)
 }
 
